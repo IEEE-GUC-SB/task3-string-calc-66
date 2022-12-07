7ddcf906c3e6c062d4bc57f3b1b15d02d9cf9fa9
MostafaAdel6822 <106386817+MostafaAdel6822@users.noreply.github.com>
2022-12-08 01:45:44 +0300

    🔄 refactored test1

--- Calc.java
+++ Calc.java
@@ -3,10 +3,19 @@ package task3;
 public class Calc {
 
 	public int getResult(String s) {
+		int sum=0;
 		if (s.length()==0)
 			return 0;
-		return 1;
+		else  {
+			String numbers[] = s.split(","); 
+			for (String number : numbers) {  // loop through all the number in the string array
+			    Integer n = Integer.parseInt(number);  // parse each number
+				System.out.println(n);
+			    sum += n;     // sum the numbers
+			  }	
+			return sum;
+		}
+	
 	}
 	
-
 }
