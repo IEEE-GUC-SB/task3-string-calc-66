2629c42fd830f608bc1e9b099949a8401e6cbbd4
MostafaAdel6822 <106386817+MostafaAdel6822@users.noreply.github.com>
2022-12-09 01:35:06 +0300

    ✅ made test4 pass

--- Calc.java
+++ Calc.java
@@ -1,9 +1,13 @@
 package task3;
 
+import static org.junit.Assert.assertThrows;
+
 public class Calc {
 
-	public int getResult(String s) {
+	public int getResult(String s) throws Exception {
+
 		int sum=0;
+		int x =0;
 		if (s.length()==0)
 			return 0;
 		else  {
@@ -18,10 +22,12 @@ public class Calc {
 			
 			if (count>=1) 
 				return 0;
-			
 			String numbers[] = s.split(","); 
 			for (String number : numbers) {  // loop through all the number in the string array
 			    Integer n = Integer.parseInt(number);  // parse each number
+			    if(n<0) {
+			    	throw new Exception("negatives not allowed");			    	
+			    }
 				System.out.println(n);
 			    sum += n;     // sum the numbers
 			  }	
--- string_calc.java
+++ string_calc.java
@@ -1,13 +1,17 @@
 package task3;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
+
 public class string_calc {
 	
+	
 	@Test
-	public void test1(){
+	public void test1() throws Exception{
 		Calc c = new Calc();
 		int r = 6;
 		assertEquals(r,c.getResult("1,2,3"));
@@ -15,26 +19,33 @@ public class string_calc {
 	}
 
 	@Test
-	public void test2(){
+	public void test2() throws Exception{
 		Calc c = new Calc();
 		int r = 8;
 		assertEquals(r,c.getResult("1\n2,5"));
 		
 	}
 	@Test
-	public void test3(){
+	public void test3() throws Exception{
 		Calc c = new Calc();
 		int r = 0;
 		
 		assertEquals(r,c.getResult("1,\n"));
 		
 	}
+
 	@Test
-	public void test4(){
+	public void test4() {
 		Calc c = new Calc();
-		int r = 100;
-		assertEquals(r,c.getResult("-10,3"));
-		
+	    Exception exception = assertThrows(Exception.class, () -> {
+	        c.getResult("-1,2,3");
+	    });
+
+	    String expectedMessage = "negatives not allowed";
+	    
+	    String actualMessage = exception.getMessage();
+	    //System.out.println(actualMessage);
+	    assertTrue(actualMessage.contains(expectedMessage));
 	}
 	
 	
